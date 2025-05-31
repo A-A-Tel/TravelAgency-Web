2a4ca466d0d4f866e59721c1ee4aca304ec7092d
a-a-tel <anouaranthonytel@gmail.com>
2025-05-31 15:38:23 +0200

    Multiple changes
    - UPDATE .gitignore
    
    - UPDATE init.sql
    
      - ADD locations table
    
      - ADD travels table
    
      - ADD bookings table

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,37 @@
 CREATE TABLE IF NOT EXISTS `users`
 (
     `id`         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name`       VARCHAR(64)  NOT NULL UNIQUE,
+    `name`       VARCHAR(64)  NOT NULL,
+    `email`      VARCHAR(320) NOT NULL UNIQUE,
     `pass`       VARCHAR(256) NOT NULL,
     `admin`      BOOLEAN      NOT NULL DEFAULT FALSE,
     `created_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `locations`
+(
+    `id`         BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name`       VARCHAR(32) NOT NULL,
+    `created_at` TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `travels`
+(
+    `id`          BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `location_id` BIGINT       NOT NULL,
+    `name`        VARCHAR(32)  NOT NULL UNIQUE,
+    `description` VARCHAR(140) NOT NULL,
+    `price_int`   INT          NOT NULL,
+    `price_dec`   INT          NOT NULL,
+    `created_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `bookings`
+(
+    `id`         BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `booker_id`  BIGINT    NOT NULL,
+    `travel_id`  BIGINT    NOT NULL,
+    `date`       DATE      NOT NULL,
+    `approved`   BOOLEAN   NOT NULL DEFAULT FALSE,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
